a88b1b3e34fbd42441c0e92ce37b98e146553426
Laura Sofía Victoria Santillán Arévalo <61711@upn.pe>
2017-12-04 00:02:27 -0500

    Respuestas

--- reducers/AgentReducer.js
+++ reducers/AgentReducer.js
@@ -271,19 +271,19 @@ const mockContent = [
   },
   {
     id: 3,
-    content: 'Te convocamos a nuestro proceso de admisión por concurso público para el ciclo académico Pre Grado Tradicional 2018-1.'
+    content: ''
     },
   {
     id: 4,
-    content: ' '
+    content: 'Te convocamos a nuestro proceso de admisión por concurso público para el ciclo académico Pre Grado Tradicional 2018-1.'
   },
   {
     id: 5,
-    content: ' '
+    content: 'Nuestro proceso de admisión se apertura dos veces al año. Estamos actualizando el calendario '
   },
   {
     id: 6,
-    content: ' '
+    content: 'Las preguntas del examen de admisión se orientan a evaluar tus capacidades y habilidades personales como postulante'
   },
   {
     id: 7,
@@ -291,59 +291,59 @@ const mockContent = [
   },
   {
     id: 8,
-    content: ' '
+    content: '  Lógica: proposiciones, inferencias. Razonamiento inductivo, razonamiento deductivo. Aritmética: proporciones, porcentajes, intereses.  Álgebra: expresiones algebraicas, factorización, ecuaciones. Geometría: geometría plana. '
   },
   {
     id: 9,
-    content: ' '
+    content: 'Autoconocimiento. Nociones básicas de motivación, aprendizaje y control emocional. Eficacia personal. '
   },
   {
     id: 10,
-    content: ' '
+    content: ' Conceptos básicos de sociedad y ciudadanía. Autoestima y satisfacción personal.'
   },
   {
     id: 11,
-    content: ' '
+    content: 'Situación comunicativa. Corrección idiomática. Comprensión de textos. Gramática. '
   },
   {
     id: 12,
-    content: ' '
+    content: 'Sinonimia contextual. Antonimia contextual. Conectores lógicos. Analogías.'
   },
   {
     id: 13,
-    content: ' '
+    content: ' Conceptos de tecnología a nivel usuario. Uso de sistema operativo y ofimática básica.'
   },
   {
     id: 14,
-    content: ' '
+    content: ' Geografía del Perú y Latino América. Historia del Perú. Historia contemporánea reciente. Educación cívica. Nociones básicas de responsabilidad social.'
   },
   {
     id: 15,
-    content: ' '
+    content: ' Podrás ingresar a nuestra universidad a través de las siguientes modalidades: 1. Ingreso regular. 2. Si quieres trasladarte desde otra universidad. 3. Si eres egresado de ITN-Cibertec (Solo en Trujillo). 4. Si eres egresado de un Instituto. 5. Si eres egresado de otra universidad.'
   },
   {
     id: 16,
-    content: ' '
+    content: 'Puedes ingresar bajo esta modalidad si eres egresado de colegio nacional, particular y no escolarizados del Perú y del extranjero. Para esto deberás presentar la documentación requerida y cumplir los siguientes requisitos:Certificado original de estudios secundarios, Copia de DNI o partida de nacimiento original provisional.'
   },
   {
     id: 17,
-    content: ' '
+    content: 'Si procedes de otra universidad de nuestro país (nacional o privada) y/o extranjera, deberás haber cursado como mínimo un ciclo en la universidad de procedencia. Para aplicar los requisitos son: Certificado original de estudios superiores, Copia de DNI, Silabus visados por el Area Academica de la Universidad de origen, Constancia de no haber sido separado de la Universidad.'
   },
   {
     id: 18,
-    content: ' '
+    content: 'Si eres egresado del ITN-Cibertec (Trujillo), puedes ingresar por esta modalidad, siguiendo el proceso de convalidación de acuerdo al plan de estudios vigente. Los requisitos son: Certificado original de estudios técnicos, Copia de DNI, Silabus visados por el Area Academica del Instituto de origen, Copia legalizada del título técnico. '
   },
   {
     id: 19,
-    content: ' '
+    content: 'Si eres egresado de algún instituto de educación superior y haz concluido tus estudios técnicos (Tres años como mínimo), puedes ingresar y convalidar según evaluación de cada facultad. Los requisitos son: Certificado original de estudios técnicos, Copia de DNI, Silabus visados por el Area Academica del Instituto de origen, Copia legalizada del título técnico.'
   },
   {
     id: 20,
-    content: ' '
+    content: 'Ingresan por esta modalidad los egresados de otras universidades con el grado de bachiller. Los requisitos son: Certificado original de estudios técnicos. Copia de DNI. Silabus visados por el Area Académica de la universidad de origen. Copia legalizada del diploma de bachiller.'
   },
   {
     id: 21,
-    content: ' '
+    content: 'Sí, contamos con programas de ayuda económica enfocados en el bienestar académico de nuestros estudiantes.'
   },
   {
     id: 22,
@@ -351,79 +351,79 @@ const mockContent = [
   },
   {
     id: 23,
-    content: ' '
+    content: 'Calificas a esta beca si eres postulante de la modalidad Ingreso Premio de Excelencia y ocupas los primeros puestos en la evaluación para Premio de Excelencia, de acuerdo a las vacantes predefinidas para cada campaña. Los requisitos que debes cumplir son: Ocupar el primer puesto en la etapa escolar, habiendo egresado del colegio con una antigüedad no mayor a dos años. Rendir la evaluación especial para Premio de Excelencia y obtener una calificación que lo ubique dentro de los cupos asignados a cada campaña. '  
   },
   {
     id: 24,
-    content: ' '
+    content: 'Es un beneficio del 10% sobre la pensión académica que te otorgamos si cumples con los siguientes requisitos: Pertenecer a un colegio nacional (de distritos donde el estado desarrolla programas sociales). Deberás tener una nota mínima de 15 en tu promedio de 1ro a 5to de secundaria. En caso estés en 5to de secundaria y todavía no dispongas de tus notas finales, puedes usar el promedio de 1ro a 4to de secundaria. En caso seas egresado, se requiere el promedio de 1ro a 5to de secundaria. Este beneficio alcanza a los egresados de estos colegios de cualquier año. Para mantener este beneficio, debes obtener una nota mínima de 14 cada ciclo.'
   },
   {
     id: 25,
-    content: ' '
+    content: 'Si tienes un hermano que estudia en UPN, contarás con el descuento de Pensión Hermanos, servicio que brindamos cuando los beneficiarios son estudiantes nuevos o regulares del programa Pre-Grado Tradicional. Este descuento deberá ser solicitado después del registro de cursos durante el periodo de matrícula. '
   },
   {
     id: 26,
-    content: ' '
+    content: 'Si todavía no decides que estudiar, te ayudamos a despejar tus dudas a través del servicio de orientación vocacional. A través de diversas evaluaciones podrás identificar cuáles son tus preferencias académicas que te permitirán elegir de forma adecuada tu carrera.'
   },
   {
     id: 27,
-    content: ' '
+    content: 'Para poder orientarte necesitamos seguir cuatro pasos específicos: Evaluación. Entrevista personal. Coordinaremos una visita guiada. Retroalimentación.'
   },
   {
     id: 28,
-    content: ' '
+    content: ' Si sientes que la carrera que elegiste no es lo tuyo, no te preocupes, te ayudaremos a encontrar la correcta.'
   },
   {
     id: 29,
-    content: ' '
+    content: ' Así es, Working Adult es el programa de carreras universitarias para adultos que trabajan, recuerda que solo aplicas a este programa si eres mayor de 24 años, con experiencia laboral comprobada.'
   },
   {
     id: 30,
-    content: ' '
+    content: ' Experimenta tu profesión con nuestros talleres vivenciales, donde conocerás de cerca cómo se desarrollan los profesionales de la carrera que tienes en mente. A través de las diversas actividades que tenemos preparadas para ti, vivirás tu primera experiencia académica.'
   },
   {
     id: 31,
-    content: ' '
+    content: ' A lo largo del año académico programamos diversos talleres vivenciales, lo cuales comunicamos oportunamente a través de nuestra web.'
   },
   {
     id: 32,
-    content: ' '
+    content: ' Nuestros talleres vivenciales no tienen costo. '
   },
   {
     id: 33,
-    content: ' '
+    content: ' Puedes indicarnos tu interés de participar, a través de nuestros formularios. Nosotros nos comunicaremos contigo.'
   },
   {
     id: 34,
-    content: ' '
+    content: ' Claro, puedes asistir con tus padres.'
   },
   {
     id: 35,
-    content: ' '
+    content: ' Sí, pero debes inscribirte previamente. '
   },
   {
     id: 36,
-    content: ' '
+    content: ' A través de nuestras charlas informativas queremos explicarte por qué somos tu mejor opción en educación superior. Te invitamos a que nos visites en nuestros diferentes campus, y así, conozcas nuestra historia, modelo educativo, competencias y ventajas diferenciales.'
   },
   {
     id: 37,
-    content: ' '
+    content: ' A lo largo del año académico programamos diversas charlas informativas, las cuales comunicamos oportunamente a través de nuestra web.'
   },
   {
     id: 38,
-    content: ' '
+    content: ' Nuestras charlas informativas no tienen costo.'
   },
   {
     id: 39,
-    content: ' '
+    content: ' Puedes indicarnos tu interés de participar, a través de nuestros formularios. Nosotros nos comunicaremos contigo. '
   },
   {
     id: 40,
-    content: ' '
+    content: ' Claro, puedes asistir con tus padres.'
   },
   {
     id: 41,
-    content: ' '
+    content: ' Sí, pero debes inscribirte previamente'
   }
   
   // Como esto está comentado, cuando seleccionemos la opción de SUBREQUISITO-2 nos saldrá un mensaje de que no hay información en vez de crashear
